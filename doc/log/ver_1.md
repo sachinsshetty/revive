c9a791335d573c064edf3264297150f7e92d2fa5
Sachin Shetty <26170834+sachinsshetty@users.noreply.github.com>
2022-05-18 01:33:21 +0200

    Setup project for Ver 1.1

---
+++ doc/log/ver_1.md
@@ -0,0 +1,7 @@
+# Version 1
+
+|No. | Step | Status | Date |
+|--|--|--|--|
+|1.| Run main class from Server.java eclipse control panel| Done | 18-May-22 |
+|2.| Generate artifacts for deployment |||
+|3.| Create Test Suite|||
---
+++ src/main/java/slabs/tech/avti/Server.java
@@ -0,0 +1,13 @@
+package slabs.tech.avti;
+
+public class Server {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Simulator for Autonomous Vehicle Tech Infrasture (AVTI)");
+		
+
+	}
+
+}
