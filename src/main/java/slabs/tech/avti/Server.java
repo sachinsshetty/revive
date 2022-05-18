9d832d1800996598beadc55ac9a34538e915e31f
Sachin Shetty <26170834+sachinsshetty@users.noreply.github.com>
2022-05-18 10:03:58 +0200

    Create Driver Class

--- src/main/java/slabs/tech/avti/Server.java
+++ src/main/java/slabs/tech/avti/Server.java
@@ -1,11 +1,20 @@
 package slabs.tech.avti;
 
+import slabs.tech.avti.vehicle.Driver;
+
 public class Server {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Simulator for Autonomous Vehicle Tech Infrasture (AVTI)");
+		
+		long driverId = 1;
+		String driverName = "DriverName";
+		
+		Driver driver = new Driver(driverId, driverName);
+		
+		System.out.println(driver);
 		
 
 	}
---
+++ src/main/java/slabs/tech/avti/vehicle/Driver.java
@@ -0,0 +1,61 @@
+package slabs.tech.avti.vehicle;
+
+public class Driver {
+
+	public Driver(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	private long id;
+	private String name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		String driverString = " ID - " + getId() + " , Name - " + getName();
+		return driverString;
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+	}
+
+}
