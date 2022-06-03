b4b27f28f94ce483361c0042ec5b75f08632c72f
sachin <sachins911991@gmail.com>
2022-06-03 06:50:46 +0200

    fix structure for test setup

---
+++ server/src/test/java/com/slabstech/revive/web/SetupControllerTest.java
@@ -0,0 +1,28 @@
+package com.slabstech.revive.web;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SetupControllerTest {
+
+    @Test
+    public void getAllSetups() {
+    }
+
+    @Test
+    public void getSetupById() {
+    }
+
+    @Test
+    public void createSetup() {
+    }
+
+    @Test
+    public void updateSetup() {
+    }
+
+    @Test
+    public void deleteSetup() {
+    }
+}
