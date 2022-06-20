b989cfd7e074815595b8377bbf404441382966b5
sachin <sachins911991@gmail.com>
2022-06-20 21:08:56 +0200

    Add build task to create all images

--- servers/spring_boot/src/main/java/com/slabstech/revive/server/springboot/web/SetupController.java
+++ servers/spring_boot/src/main/java/com/slabstech/revive/server/springboot/web/SetupController.java
@@ -10,7 +10,6 @@ import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.Validator;
 
-import com.slabstech.codechallenge.house.house_man.persistence.model.Home;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
